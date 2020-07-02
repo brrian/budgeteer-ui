29fa0664791f256cc1f87f1b5b5d4284673589ad
Brian Wang <brrian@gmail.com>
2020-07-02 17:12:07 -0500

    Revert "Disable secure cookie flag"
    This reverts commit 7a891f1046ae1fa5c0519123e6675bf57051bd12.

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ Amplify.configure({
       domain: process.env.REACT_APP_COGNITO_COOKIE_DOMAIN,
       path: '/',
       expires: 365,
-      secure: false,
+      secure: process.env.REACT_APP_COGNITO_SECURE !== 'false',
     },
   },
 });
