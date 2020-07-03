7cc69b9d1905257539e5eb4f837a85a7d6eaa7fb
Brian Wang <brrian@gmail.com>
2020-07-03 10:29:06 -0500

    Add password reset flow

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,5 @@ export const PAGE_LOGIN = '/login';
 export const PAGE_REGISTER = '/register';
 export const PAGE_REGISTER_RESEND = '/register/resend';
 export const PAGE_REGISTER_VERIFY = '/register/verify';
+export const PAGE_RESET_PASSWORD = '/forgot-password/reset';
 export const PAGE_TRANSACTIONS = '/';
