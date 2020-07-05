7a85f1c6b43af6ce88c34c1adb1f1869affcf0e2
Brian Wang <brrian@gmail.com>
2020-07-05 10:04:33 -0500

    Add long press to toggle budget menu

--- src/pages/TransactionsPage/index.tsx
+++ src/pages/TransactionsPage/index.tsx
@@ -13,23 +13,48 @@ const TransactionsPage: FC = () => {
   const auth = useAuth();
 
   const isExpanded = useRef(false);
+  const longPressRef = useRef<number>();
 
   const [{ height }, set] = useSpring(() => ({
     config: config.stiff,
     height: COLLAPSED_HEIGHT,
   }));
 
-  const handleExpandClick = () => {
+  const handleBudgetDoubleClick = () => {
+    toggleBudgetMenu();
+  };
+
+  const handleBudgetTouchStart = () => {
+    longPressRef.current = window.setTimeout(() => {
+      toggleBudgetMenu();
+    }, 300);
+  };
+
+  const handleBudgetTouchEnd = () => {
+    if (longPressRef.current) {
+      window.clearTimeout(longPressRef.current);
+    }
+  };
+
+  const toggleBudgetMenu = () => {
     const newValue = isExpanded.current ? COLLAPSED_HEIGHT : EXPANDED_HEIGHT;
 
     set({ height: newValue });
+
+    navigator.vibrate(70);
 
     isExpanded.current = !isExpanded.current;
   };
 
   return auth.isReady ? (
     <div className={styles.container}>
-      <animated.div className={styles.budget} onDoubleClick={handleExpandClick} style={{ height }}>
+      <animated.div
+        className={styles.budget}
+        onDoubleClick={handleBudgetDoubleClick}
+        onTouchEnd={handleBudgetTouchEnd}
+        onTouchStart={handleBudgetTouchStart}
+        style={{ height }}
+      >
         <BudgetCategories />
       </animated.div>
       <animated.div className={styles.transactions} style={{ top: height }}>
