47bee9a914769fdd9a5902bf4c9de9ab4db37a48
Brian Wang <brrian@gmail.com>
2020-12-08 14:12:18 -0600

    Fix month marker in budget categories

--- src/components/BudgetCategories/Categories.tsx
+++ src/components/BudgetCategories/Categories.tsx
@@ -1,4 +1,4 @@
-import { getDate, getDaysInMonth } from 'date-fns';
+import { getDaysInMonth } from 'date-fns';
 import React, { FC } from 'react';
 import styles from './Categories.module.scss';
 import Category from './Category';
@@ -6,15 +6,16 @@ import { BudgetCategory } from './models';
 
 interface CategoriesProps {
   categories: BudgetCategory[];
-  date: Date;
   isCurrentMonth: boolean;
 }
 
-const Categories: FC<CategoriesProps> = ({ categories, date, isCurrentMonth }) => {
+const Categories: FC<CategoriesProps> = ({ categories, isCurrentMonth }) => {
   const daysInMonth = getDaysInMonth(new Date());
   const percentPerDay = 100 / daysInMonth;
 
-  const monthProgress = isCurrentMonth ? Math.round((getDate(date) / daysInMonth) * 100) : 100;
+  const monthProgress = isCurrentMonth
+    ? Math.round((new Date().getDate() / daysInMonth) * 100)
+    : 100;
 
   return (
     <div className={styles.categories}>
